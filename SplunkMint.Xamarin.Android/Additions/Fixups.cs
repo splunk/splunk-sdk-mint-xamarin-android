9c394c798b8e61d8bb9a1316f94d31011056609b
George Taskos <gtaskos@splunk.com>
2014-10-22 13:56:09 -0400

    Initiali commit.

---
+++ SplunkMint.Xamarin.Android/Additions/Fixups.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Splunk.Mint.Network 
+{
+	public partial class Counter
+	{
+		protected override global::Java.Lang.Object RawValue 
+		{
+			get{
+				return RawValue2;
+			}
+		}
+
+		public global::Java.Lang.Long Value 
+		{
+			get{
+				return RawValue2;
+			}
+		}
+
+	}
+
+	public partial class Timer
+	{
+		protected override global::Java.Lang.Object RawValue 
+		{
+			get{
+				return RawValue2;
+			}
+		}
+
+		public global::Java.Lang.Long Value 
+		{
+			get{
+				return RawValue2;
+			}
+		}
+	}
+}
---
+++ SplunkXamarinClient/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Splunk.Mint;
+
+namespace SplunkXamarinClient
+{
+	[Activity (Label = "SplunkXamarinClient", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Mint.InitAndStartSession(Application.Context, "3520f5c9");
+			Mint.EnableDebug ();
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				string a = null;
+				a.ToString();
+			};
+		}
+	}
+}
